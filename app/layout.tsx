5ba49080cb8c86d92a07096ca2752a4a0053b5e4
Artur023 <artur.023@mail.ru>
2024-10-30 21:05:12 +0400

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,30 @@
-
 import type { Metadata } from "next";
-import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
-  title: "CAD Exchanger",
-  description: "CAD Exchanger test ",
+  title: "Never Gonna GYU - Welcome",
+  description: "Never gonna make you cry, never gonna say goodbye.",
+  openGraph: {
+    type: "website",
+    url: "https://yourwebsite.com",
+    title: "Never Gonna GYU",
+    description: "Never gonna make you cry, never gonna say goodbye..",
+    images: [
+      {
+        url: "https://yourwebsite.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Never Gonna GYU",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Never Gonna GYU",
+    description: "Never gonna make you cry, never gonna say goodbye..",
+    images: ["https://yourwebsite.com/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -16,10 +34,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head></head>
       <body>
-          <Header />
-          <main>{children}</main>
-          <Footer />
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
